b4fd86517df4e0c71b5cdb20eb05eaf6ac249c7b
Johannes Mols <johannes.mols@kraftvaerk.com>
2022-10-16 13:21:21 +0200

    Trying to make it OO

--- src/test/resources/biginteger/BigInteger.java
+++ src/test/resources/biginteger/BigInteger.java
@@ -1,5 +1,16 @@
 class BigIntTest {
-    public static void main(String[] a) {
+    public static void main(String[] args) {
+        BigInteger a;
+        BigInteger b;
+
+        // Useless variables that are only needed because methods have to return something
+        boolean aux01;
+        boolean aux02;
+        boolean aux03;
+
+        int c;
+        int c2;
+
         // Define base of values that will be represented by the integers (defined in configs.hpp of Zilch source)
         // For 64-bit signed integers: (2^63)-1. Range of -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
         // For 32-bit signed integers: (2^31)-1. Range of -2,147,483,648 to 2,147,483,647
@@ -8,11 +19,24 @@ class BigIntTest {
         // I.e. (2<<14)-1 for 16-bit, (2<<30)-1 for 32-bit, (2<<62)-1 for 64-bit
         int base = (2 << 62) - 1;
 
-        // Read the number of digits and whether it is negative from the file
-        // Digits need to be entered in Big-Endian order
-        int size;
-        boolean is_negative;
-        int[] digits;
+        a = new BigInteger();
+        aux01 = a.Init();
+
+        b = new BigInteger();
+        aux02 = b.Init();
+
+        Prover.answer(base);
+    }
+}
+
+class BigInteger {
+    int size;
+    int[] digits;
+    boolean is_negative;
+    
+    // Read the number of digits and whether it is negative from the file
+    // Digits need to be entered in Big-Endian order
+    public boolean Init() {
         int digit;
         int i = 0;
         size = PublicTape.read();
@@ -26,11 +50,31 @@ class BigIntTest {
             digits[i] = digit;
             i++;
         }
-
-        // Does not print out negative numbers correctly (treats them not as 2-complements, but regular binary)
-        System.out.println(digits[0]);
-        System.out.println(digits[1]);
-
-        Prover.answer(base);
+        return true;
     }
+
+    public int GetLength() {
+        return digits.length;
+    }
+
+    public boolean Add(BigInteger other) {
+        return true;
+    }
+
+    public boolean Subtract(BigInteger other) {
+        return true;
+    }
+
+    public boolean Multiply(BigInteger other) {
+        return true;
+    }
+
+    public boolean Divide(BigInteger other) {
+        return true;
+    }
+
+    // Apparently max. 6 functions in a class?????????
+    /*public boolean Modulo(BigInteger other) {
+        return true;
+    }*/
 }
