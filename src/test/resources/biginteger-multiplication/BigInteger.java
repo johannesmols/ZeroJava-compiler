3a000717f562effa44f3a17bdae37633f7cc4e28
tabs-of-course <juriborren+tabs-of-course@gmail.com>
2022-11-07 18:40:05 +0100

    compiles but runtime exception

--- src/test/resources/biginteger-multiplication/BigInteger.java
+++ src/test/resources/biginteger-multiplication/BigInteger.java
@@ -1,102 +1,98 @@
 class BigIntTest {
-    public static void main(String[] a) {
-        // Define base of values that will be represented by the integers (defined in configs.hpp of Zilch source)
+    public static void main(String[] input_string) {
+        // Define base of values that will be represented by the integers (defined in configs.hpp of Zilch source) 
         // For 64-bit signed integers: (2^63)-1. Range of -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 (9223372036854775807) (3037000499 squared)
-        // For 32-bit signed integers: (2^31)-1. Range of -2,147,483,648 to 2,147,483,647
-        // For 16-bit signed integers: (2^15)-1. Range of -32,768 to 32,767
+
         // Use bit shift operator on exponent minus one as it works for powers of 2
         // I.e. (2<<14)-1 for 16-bit, (2<<30)-1 for 32-bit, (2<<62)-1 for 64-bit
         // int base = (2 << 62) - 1;
 
-        // Store actual values
-        int[] a_num;
-        int[] b_num;
-        int[] res_num;
+        
+        // The numbers are stored in an array, where each index represents a single digit 
+        // Both numbers a and b are read from one single file, where the digits of each number..
+        // ..are prepend by the amount of digits that number contains 
+        int[] a;
+        int[] b;
+        int[] result;
 
         int carry = 0;
-        int a_val = 0;
-        int b_val = 0;
+        int a_digit_value = 0;
+        int b_digit_value = 0;
         int tmp_val = 0;
+        int rem_val = 0;
 
-
-        // Temporary values for looping, reading in values, etc.
-        int a_digits = 0;
-        int b_digits = 0;
-        int res_digits = 0;
+        int a_number_of_digits = 0;
+        int b_number_of_digits = 0;
+        int result_number_of_digits = 0;
 
         // Index of a digit in a number    
-        int a_i;
-        int b_i;
-        int res_i;
+        int a_digit_i;
+        int b_digit_i;
+        int result_digit_i;
 
         // int addition = 1;
         // int multiplication = 0;
 
-        // Read the number of digits and whether it is negative from the file
-        // Digits need to be entered in Little-Endian order
-        a_digits = PublicTape.read();
-        a_num = new int[a_digits];
-        while (a_i < a_digits) {
-            digits_a[a_i] = PublicTape.read();
-            a_i++;
+        a_digit_i = 0;
+        a_number_of_digits = PublicTape.read();
+        a = new int[a_number_of_digits];
+        while (a_digit_i < a_number_of_digits) {
+            a[a_digit_i] = PublicTape.read();
+            a_digit_i++;
         }
-        a_i = 0;
+        a_digit_i = 0;
 
-        b_digits = PublicTape.read();
-        b_num = new int[b_digits];
-        while (b_i < b_digits) {
-            digits_b[b_i] = PublicTape.read();
-            b_i++;
+        b_digit_i = 0;
+        b_number_of_digits = PublicTape.read();
+        b = new int[b_number_of_digits];
+        while (b_digit_i < b_number_of_digits) {
+            b[b_digit_i] = PublicTape.read();
+            b_digit_i++;
         }
-        b_i = 0;
+        b_digit_i = 0;
 
-       
-   
-        if ((a_digits) > (b_digits))
-            res_digits = a_digits;
-        else
-            res_digits = b_digits + 1;
+        result_number_of_digits = ((a_number_of_digits) >= (b_number_of_digits)) ? a_number_of_digits + 1 : b_number_of_digits + 1;
 
-        res_num = new int[res_digits];
+        result = new int[result_number_of_digits];
+ 
+        while (result_digit_i < result_number_of_digits) {
+            a_digit_value = a[a_digit_i];
+            a_digit_i++;
+            b_digit_value = b[b_digit_i];
+            b_digit_i++;
 
-
-
-        // Addition of A and B
-        
-        while (res_i < res_digits) {
-            a_val = a_num[a_i];
-            a_i++;
-            b_val = b_num[b_i];
-            b_i++;
-
-            tmp_val = (a_val + b_val) + carry;
+            tmp_val = (a_digit_value + b_digit_value) + carry;
+            
             if (tmp_val > 9) { 
-                // Overflow occured
-                res_num[res_i] = tmp_val - 10;
-                carry = 1; // Set carry to 1
+                result[result_digit_i] = tmp_val - 10;
+                carry = 1; 
             } else {
-                res_num[res_i] = tmp_val;
+                result[result_digit_i] = tmp_val;
                 carry = 0;
             }
-            res_i++;
+            result_digit_i++;
         }
+
+
         if (carry > 0)
-            res_num[res_digits] = carry;
+            result[result_number_of_digits] = carry;
         else
             // Accessing this index will otherwise throw a runtime exception (uninitialized)
-            res_num[res_digits] = 0;
+            result[result_number_of_digits] = 0;
     
 
         // Print out resulting digits in base 10
-        System.out.println(res_digits);
-        res_i = 0;
-        while (res_i < res_digits) {
+        System.out.println(result_number_of_digits);
+        result_digit_i = 0;
+        while (result_digit_i < result_number_of_digits) {
            
-            System.out.println(res_num[res_i]);
-            res_i++;
+            System.out.println(result[result_digit_i]);
+            result_digit_i++;
         }
 
-        Prover.answer(res_num[0]);
+        Prover.answer(result[0]);
     }
     
 }
+
+// cd ZeroJava-compiler/; java -jar target/zerojava-compiler-1.0.jar src/test/resources/biginteger-multiplication/BigInteger.java; cd ..; ./zilch --asm ZeroJava-compiler/src/test/resources/biginteger-multiplication/BigInteger.zmips
